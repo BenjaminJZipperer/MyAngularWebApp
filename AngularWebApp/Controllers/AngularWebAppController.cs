18b5d4d9d66967bf1311c2bf1c4302d18e6c47af
Benjamin Zipperer <benjamin-zipperer@web.de>
2021-01-08 15:52:00 +0100

    Succesfully implemented Entity FW Models into App

---
+++ AngularWebApp/Controllers/AngularWebAppController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AngularWebApp.Models;
+// Namespace for Controller
+namespace AngularWebApp.Controllers
+{
+    // Benji is busy (malcomx ohne Schema)
+    [RoutePrefix("Api/Employee")]
+    public class AngularWebAppController : ApiController
+    {
+        //www.microfocus.com/documentation/silk-test/200/en/silktestworkbench-help-en/SILKTEST-7FFBB86A-CREATINGNEWSQLSERVERADMINUSER-TSK.html
+        [HttpGet]
+        [Route("AllEmployeeDetails")]
+        public IHttpActionResult GetEmaployee()
+        {
+            return null;
+        }
+    }
+}
